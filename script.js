89881f239c37cec0cd46ec062ef78930a06309f9
Bruno Souza <80255960+brunoalksouza@users.noreply.github.com>
2023-02-16 14:18:10 +0000

    responsive site

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const btnBurguer = document.querySelector("#menu-buguer-open");
+const menuWrapper = document.querySelector("#mobile-menu-wrapper");
 
+function mobileMenu() {
+  btnBurguer.classList.toggle("mobile-menu");
+  menuWrapper.classList.toggle("none");
+
+  console.log("sss");
+}
